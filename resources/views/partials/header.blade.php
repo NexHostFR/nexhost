1a37e782035252f209026f62c8a9fb6d5df8f39c
Théo Killian <theo.killian5700@Gmail.com>
2024-12-17 20:31:26 +0100

    Modification header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,21 +1,18 @@
 <header class="flex items-center p-3">
-    <div class="flex flex-wrap items-center justify-between w-full">
+    <div class="flex flex-wrap items-center justify-between w-screen">
         <div class="flex items-center">
             <a href="/" class="no-underline">
                 <img src="/asset/img/logo.png" alt="Logo de NxHost" class="h-16">
             </a>
         </div>
-        <ul class="flex space-x-4"> 
-            @foreach (config("global.categorie") as $category)
-                <li>
-                    <a href="/produit/{{ $category['url'] }}" class="text-gray-900 hover:underline">
-                        {{ $category["title"] }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
-        <a target="_blank" href="https://manager.NxHost.fr">
-            <button class="bg-gray-700 text-white py-2 px-4 rounded hover:bg-gray-800">Espace client</button>
-        </a>
+        <div class="flex flex-row items-center justify-between w-1/2">
+            <ul class="flex space-x-4"> 
+                <li><span>Nos produit</span></li>
+                <li><span>Nos ressource</span></li>
+            </ul>
+            <a target="_blank" href="https://manager.NxHost.fr">
+                <button class="bg-gray-700 text-white py-2 px-4 rounded hover:bg-gray-800">Espace client</button>
+            </a>
+        </div>  
     </div>
 </header>
