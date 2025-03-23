4d2ad31c109de9c8f33ae1d385b89c9a8707fdac
Théo Killian <theo.killian5700@Gmail.com>
2025-03-23 10:36:27 +0100

    Gestion Categorie faite'

--- app/Providers/HeaderServiceProvider.php
+++ app/Providers/HeaderServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Categorie;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -22,42 +23,16 @@ class HeaderServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::composer('partials.header', function($view) {
-            $CategoriesProduct = array(
-                array(
-                    "name" => "Web et Domaines",
-                    "type" => "web",
-                ),
-                array(
-                    "name" => "Gaming",
-                    "type" => "gaming",
-                ),
-            );
-            $GroupProduct = array(
-                "web" => array(
-                    /*"domaines" => array(
-                        "name" => "Domaines",
-                        "description" => "Trouvez le nom de domaine idéal",
-                        "slug" => "/domaines",
-                    ),*/
-                    "hebergement" => array(
-                        "name" => "Hébergement Web",
-                        "description" => "Hébergez votre site web",
-                        "slug" => "/hebergement/web",
-                    )
-                ),
-                "gaming" => array(
-                    "fivem" => array(
-                        "name" => "Fivem",
-                        "description" => "Serveur Fivem",
-                        "slug" => "/gaming/fivem",
-                    ),
-                    "minecraft" => array(
-                        "name" => "Minecraft",
-                        "description" => "Serveur Minecraft",
-                        "slug" => "/gaming/minecraft",
-                    )
-                    ),
-            );
+            $categorieListe = Categorie::all();
+            $CategoriesProduct = [];
+            $GroupProduct = [];
+            foreach($categorieListe as $categorie) {
+                if(is_null($categorie->categorie_parent)) {
+                    array_push($CategoriesProduct, $categorie); 
+                } else {
+                    array_push($GroupProduct, $categorie);
+                }
+            }
             $view->with('CategoriesProduct', $CategoriesProduct);
             $view->with('GroupProduct', $GroupProduct);
         });
